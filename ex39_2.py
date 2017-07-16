523fb128b57f8d276842c0a5bf58bb0fcb509844
GlennLewis <glelewis90@gmail.com>
2017-07-15 22:24:15 -0500

    Exercise 39
    Exercise 39 and assignment

---
+++ ex39_2.py
@@ -0,0 +1,14 @@
+pokemon_type = {
+'Pikachu': 'Electric',
+'Charmander': 'Fire',
+'Squirtle': 'Water',
+'Bulbasaur': 'Grass'
+}
+
+print "Current Pokemon: ", pokemon_type
+
+print "Adding Evee, Cyndiquil, Chicaritta, and Totodile"
+
+pokemon_type.update({'Evee': 'Normal', 'Cyndiquil': 'Fire', 'Chicaritta': 'Grass', 'Totodile': 'Water'})
+
+print "Updated Pokemon: ", pokemon_type
